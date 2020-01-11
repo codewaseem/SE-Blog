2834e3897263948a586172587582a4151e0edc12
Waseem Ahmed <codewaseem@gmail.com>
2020-01-11 19:56:53 +0530

    add emotion, my first blog post content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return (
+    <>
+      <h2>Post Title</h2>
+      <p>
+        Fugiat exercitation ullamco dolor ex culpa esse anim ea tempor laboris
+        dolor proident. Ea est ut magna adipisicing velit et consectetur sint
+        culpa. Aute ex do cillum do aute elit eiusmod cillum minim enim nisi.
+      </p>
+      <p>Add another paar</p>
+    </>
+  )
+}
