4cb6789a543db6c57a487be50a24ee0f3ed9ddc2
Waseem Ahmed <codewaseem@gmail.com>
2020-01-11 20:08:31 +0530

    add stie metadata and mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,26 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ["gatsby-plugin-emotion"],
+  siteMetadata: {
+    title: "The Software Engineering Blog",
+    description:
+      "In this blog you will find posts on topics like software engineering, algorithms, data structures, programming languages, blockchian, artificial intelligence, web development, etc",
+  },
+
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+  ],
 }
