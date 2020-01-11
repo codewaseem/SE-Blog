5a355a11df8b47d05c8dc6a490d013ad8a142d24
Waseem Ahmed <codewaseem@gmail.com>
2020-01-11 21:06:44 +0530

    add useSiteMetadata hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import React from "react"
+import { useSiteMetadata } from "../hooks/useSiteMetadata"
 
 export default () => {
+  const { title, description } = useSiteMetadata()
+
   return (
     <>
-      <h2>Post Title</h2>
-      <p>
-        Fugiat exercitation ullamco dolor ex culpa esse anim ea tempor laboris
-        dolor proident. Ea est ut magna adipisicing velit et consectetur sint
-        culpa. Aute ex do cillum do aute elit eiusmod cillum minim enim nisi.
-      </p>
-      <p>Add another paar</p>
+      <h2>{title}</h2>
+      <p>{description}</p>
     </>
   )
 }
