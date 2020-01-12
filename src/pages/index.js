126c3a6e5ecfd17832e9ac341edab07bffd7e3a8
Waseem Ahmed <codewaseem@gmail.com>
2020-01-12 20:47:50 +0530

    show blog posts on the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,33 @@
 import React from "react"
 
-export default () => {
-  return <div>Content related to index page</div>
+export default ({ data }) => {
+  return (
+    <>
+      {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
+        <>
+          <h1>{frontmatter.title}</h1>
+          <p>{frontmatter.date}</p>
+          <p>{excerpt}</p>
+        </>
+      ))}
+    </>
+  )
 }
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+`
