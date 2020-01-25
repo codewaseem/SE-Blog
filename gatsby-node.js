3cc9e91a8992998a74d2862212888849a0b746eb
Waseem Ahmed <codewaseem@gmail.com>
2020-01-25 07:21:19 +0530

    show post content

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,52 @@
 const { createFilePath } = require("gatsby-source-filesystem")
+const path = require("path")
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
   if (node.internal.type === `Mdx`) {
     const value = createFilePath({ node, getNode, basePath: "pages" })
+
     createNodeField({
       name: `slug`,
       node,
-      value,
+      value: `/posts${value}`,
     })
   }
+}
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve("src/templates/post.js")
+
+  return graphql(`
+    {
+      allMdx {
+        nodes {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    const posts = result.data.allMdx.nodes
+
+    // create page for each mdx file
+    posts.forEach(post => {
+      createPage({
+        path: post.fields.slug,
+        component: blogPostTemplate,
+        context: {
+          slug: post.fields.slug,
+        },
+      })
+    })
+  })
 }
---
+++ src/pages/posts.js
@@ -0,0 +1,44 @@
+import React from "react"
+import styled from "@emotion/styled"
+import { Link, graphql } from "gatsby"
+
+const IndexWrapper = styled.main``
+
+const PostWrapper = styled.div``
+
+export default ({ data }) => {
+  return (
+    <IndexWrapper>
+      {data.allMdx.nodes.map(({ id, excerpt, frontmatter, fields }) => (
+        <PostWrapper key={id}>
+          <Link to={fields.slug}>
+            <h1>{frontmatter.title}</h1>
+            <p>{frontmatter.date}</p>
+            <p>{excerpt}</p>
+          </Link>
+        </PostWrapper>
+      ))}
+    </IndexWrapper>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+`
